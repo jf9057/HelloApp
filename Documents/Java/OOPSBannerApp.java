f4b54c3e7cd0d679adb3b46f67951fa932163bf4
jf9057 <jf9057@srmist.edu.in>
2026-02-26 16:22:59 +0530

    UC5: Combined array declaration and initialization using String.join()

--- Documents/Java/OOPSBannerApp.java
+++ Documents/Java/OOPSBannerApp.java
@@ -1,6 +1,7 @@
 
 public class OOPSBannerApp {
-public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         String[] banner = {
                 String.join("  ", " ***** ", " ***** ", " ******", " ***** "),
